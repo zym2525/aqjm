daf15124d5700e357c0a596204de290cfb448133
zhengyiming <540361168@qq.com>
2017-10-20 17:42:20 +0800

    zym

---
+++ src/config/editorConfig.js
@@ -0,0 +1,8 @@
+export const editorConfig={
+  items:[
+    'undo', '|',
+    'cut', 'copy', 'paste','wordpaste','|',
+    'justifyleft', 'justifycenter', 'justifyright','justifyfull','selectall','indent', 'outdent', 'subscript','superscript','formatblock', 'fontname', 'fontsize', '|',
+    'forecolor', 'hilitecolor', 'bold','italic', 'underline', 'strikethrough', 'lineheight', 'removeformat','|'
+  ]
+};
